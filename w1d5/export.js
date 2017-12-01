2ea4d149f2f8bd24ddf7e847a249babad14b6f65
Arthur Di Lascio <arthur.dilascio@gmail.com>
2017-12-01 13:44:16 -0800

    adding modules exercise

---
+++ w1d5/export.js
@@ -0,0 +1,5 @@
+module.exports = {
+  sum: function(x, y){
+    console.log("x + y =", x + y );
+  }
+};
---
+++ w1d5/import.js
@@ -0,0 +1,5 @@
+var importing = require("./export");
+var x = 2;
+var y = 3;
+
+importing.sum(x, y);
