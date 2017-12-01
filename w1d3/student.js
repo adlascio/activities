55fad6ea118837cadcddb1461d0fb0a1b6ad091f
Arthur Di Lascio <arthur.dilascio@gmail.com>
2017-11-30 16:56:01 -0800

    adding sort students exercise

---
+++ w1d3/student.js
@@ -0,0 +1,27 @@
+// var students = [
+//   { id: 1, name: "bruce",    age: 40 },
+//   { id: 2, name: "zoidberg", age: 22 },
+//   { id: 3, name: "alex",     age: 22 },
+//   { id: 4, name: "alex",     age: 30 }
+// ];
+
+// students.sort(function(a,b) {
+//   console.log(a.name - b.name);
+// });
+
+
+var items = [
+  { name: 'Edward', value: 21 },
+  { name: 'Sharpe', value: 37 },
+  { name: 'And', value: 45 },
+  { name: 'The', value: -12 },
+  { name: 'Magnetic', value: 13 },
+  { name: 'Zeros', value: 37 }
+];
+
+// sort by value
+items.sort(function (a, b) {
+  return a.value - b.value;
+});
+
+console.log(items);
---
+++ w1d4/students.js
@@ -0,0 +1,26 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+
+students.sort(function compareName (a, b) {
+  var nameA = a.name.toUpperCase();
+  var nameB = b.name.toUpperCase();
+  var ageA = a.age;
+  var ageB = b.age;
+
+  if (nameA < nameB){
+    return -1;
+  }
+
+  if (nameA > nameB){
+    return 1;
+  }
+
+  return ageA - ageB;
+});
+
+console.log(students);
